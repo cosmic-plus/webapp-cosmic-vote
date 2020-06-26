b74b9a051ad141fc27ac1d4646b37a4c93f96381
MisterTicot <mister.ticot@cosmic.plus>
2020-06-26 22:06:13 +0200

    dev Fix TxParams "txHash" format decoding

--- src/lib/tx-params.tx-hash.js
+++ src/lib/tx-params.tx-hash.js
@@ -11,7 +11,7 @@ decode.transaction = async function (tx, txHash, options = {}) {
   const network = NetworkContext.normalize(options.network)
   const callBuilder = network.server.transactions().transaction(txHash)
   const record = await callBuilder.call()
-  return tx.constructor.from("txRecord", record)
+  return tx.constructor.from("txRecord", record, options)
 }
 
 /* Export */
